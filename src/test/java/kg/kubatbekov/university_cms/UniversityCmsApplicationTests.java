bf3e6a7aa8e29c085d41b6c0bd04d17aedd5c31f
Kubatbekov Nurlan <kubatbekovnurlan13@gmail.com>
2023-04-26 18:58:28 +0600

    First commit: project base build

---
+++ src/test/java/kg/kubatbekov/university_cms/UniversityCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.kubatbekov.university_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
